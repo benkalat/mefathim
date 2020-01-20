7f8f5a8929e88ce3ae9f0557b97365de4e0d5ce7
benpit <ben8652800@gmail.com>
2020-01-20 12:45:44 +0200

    ben
    script for country code auto-identification

---
+++ scripts/find_country.py
@@ -0,0 +1,20 @@
+﻿#!/home/mefath5/.local/bin/python3
+
+from os import environ
+import requests
+import json
+
+try:
+    # if "REMOTE_ADDR" in environ:
+    user_ip = environ["REMOTE_ADDR"]
+    ur = "http://api.ipstack.com/5.102.236.176?access_key=c4e56f168a3eb0af77fea9b9514bd80c"   
+    url = "http://api.ipstack.com/" +user_ip+ "?access_key=c4e56f168a3eb0af77fea9b9514bd80c"
+
+    ret = requests.get(url)
+    view = ret.json()["country_code"]
+    
+except:
+	view = json.dumps("error")
+
+print("Content-Type: text/plain\n")
+print(view)
