91d0fa7223d0a2153156fe811894e30405f08fab
moshe-eichler <eichlermoshe@gmail.com>
2020-01-22 14:18:07 +0200

    fixed start.py

--- scripts/start.py
+++ scripts/start.py
@@ -35,6 +35,5 @@ try:
         print("")
 
 except Exception as e:
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    print(exc_type, fname, exc_tb.tb_lineno)
+    print("location: login.html")
+    print("")
